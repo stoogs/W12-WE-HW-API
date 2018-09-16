015469c05a84274d9799c094aa02b73e70da53e4
Stuart Black <stuartblack@gmail.com>
2018-09-16 13:42:44 +0100

    Images now working

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -16,12 +16,19 @@ ResultView.prototype.bindEvents = function () {
 };
 
 ResultView.prototype.render = function (beers) {
+    this.resultContainer.innerHTML = '';
     //console.log(this.resultContainer);
     beers.forEach(beer => {
         //console.log(beer.name);
-        let titleHeading = document.createElement('h1');s
-        titleHeading.textContent = beer.name;
+        let titleHeading = document.createElement('h1');
+        let headline = `${beer.name} is ${beer.abv} %abv`;
+        titleHeading.textContent = headline;
         this.resultContainer.appendChild(titleHeading);
+        const img = document.createElement('img');
+        //check image url is ok
+        console.log(beer.image_url);
+        img.src = beer.image_url;
+        this.resultContainer.appendChild(img)
         //console.log(beers[4]['name'])
     })
     }
